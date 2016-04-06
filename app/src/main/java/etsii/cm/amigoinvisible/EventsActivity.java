220135994c79e8777bbde86d458ee6e230c148e9
CristinaPerea <criperrui@gmail.com>
2016-04-06 13:48:38 +0200

    Adiciones
    - Layouts de activities
    - Clase java activity events
    - Añadidos iconos de imageButton y los de aplicación en menú android.
    -Se ha cambiado de versión mínima de la api de android

--- app/src/main/java/etsii/cm/amigoinvisible/EventsActivity.java
+++ app/src/main/java/etsii/cm/amigoinvisible/EventsActivity.java
@@ -1,34 +1,27 @@
 package etsii.cm.amigoinvisible;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 import dbms.MainDBMS;
 import dbms.clsEvent;
-import dbms.clsPerson;
 
-public class MainActivity extends AppCompatActivity {
+public class EventsActivity extends AppCompatActivity {
 
     public ArrayList<String> dataToList = new ArrayList<>();
     public MainDBMS dbms = new MainDBMS();
-    String foto = new String();
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_events);
+        muestraEventos();
     }
 
-    public void btListarOnClick(View view) {
+    public void muestraEventos (){
         Thread tr = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -37,17 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
                 ArrayList<clsEvent> lstEvents = dbms.getEvents();
                 for(clsEvent objEvent : lstEvents) { dataToList.add(objEvent.getData_name()); }
-
-                /*
-                ArrayList<clsPerson> lstPersons = dbms.getPersons();
-
-                for(clsPerson objPerson : lstPersons) {
-                    dataToList.add(objPerson.getData_name() + " - " + objPerson.getData_email());
-                }
-                */
-
-                //foto = dbms.getPersonPhoto();
-
+                System.out.println("**********************Entrando en DB" + lstEvents.size());
                 runOnUiThread(
                         new Runnable() {
                             @Override
@@ -61,9 +44,8 @@ public class MainActivity extends AppCompatActivity {
         tr.start();
     }
     public void mostrarListado(){
-            ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataToList);
-            ListView lista = (ListView) findViewById(R.id.lvListado);
-            try { lista.setAdapter(adaptador); } catch (Exception e) {}
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataToList);
+        ListView lista = (ListView) findViewById(R.id.lstVwEvent);
+        try { lista.setAdapter(adaptador); } catch (Exception e) {}
     }
-
 }
