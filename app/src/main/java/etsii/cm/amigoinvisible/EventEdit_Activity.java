117b5b1223b8f51cd782a65bfed1b4f7f7773a67
Cristina Perea <criperrui@gmail.com>
2016-04-27 22:22:14 +0200

    Cambiado Layout de edición del evento y añadido Datepicker Dialog

--- app/src/main/java/etsii/cm/amigoinvisible/EventEdit_Activity.java
+++ app/src/main/java/etsii/cm/amigoinvisible/EventEdit_Activity.java
@@ -1,13 +1,21 @@
 package etsii.cm.amigoinvisible;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.Serializable;
+import java.util.Calendar;
 
 import model.ClsEvent;
 import utils.Comunicador;
@@ -15,6 +23,11 @@ import utils.Comunicador;
 public class EventEdit_Activity extends AppCompatActivity implements Serializable {
 
     private ClsEvent actualEvent;
+    private ImageButton btnDateEdit;
+    private int year_x,month_x,day_x;
+    private TextView txtFecha;
+    static final int DIALOG_ID = 0;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,7 +55,15 @@ public class EventEdit_Activity extends AppCompatActivity implements Serializabl
         super.onCreate(savedInstanceState);
         actualEvent = (ClsEvent) Comunicador.getObjeto();
         setTitle(actualEvent.getData_name());
-        setContentView(R.layout.activity_event_detail);
+        setContentView(R.layout.activity_event_edit);
+
+
+        final Calendar calendar = Calendar.getInstance();
+        year_x = calendar.get(Calendar.YEAR);
+        month_x = calendar.get(Calendar.MONTH);
+        day_x = calendar.get(Calendar.DAY_OF_MONTH);
+
+        showDialogOnButtonClick();
         showData();
     }
 
@@ -64,5 +85,31 @@ public class EventEdit_Activity extends AppCompatActivity implements Serializabl
         } catch (Exception e){;}
 
     }
+
+    public void showDialogOnButtonClick(){
+        btnDateEdit = (ImageButton) findViewById(R.id.btnEditDate);
+
+        btnDateEdit.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                showDialog(DIALOG_ID);
+            }
+        });
+    }
+
+    protected Dialog onCreateDialog(int id){
+        if(id ==DIALOG_ID){
+            return new DatePickerDialog(this,dpickerLister,year_x ,month_x,day_x);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerLister = new DatePickerDialog.OnDateSetListener(){
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+            year_x = year;
+            month_x = monthOfYear + 1;
+            day_x = dayOfMonth;
+            Toast.makeText(EventEdit_Activity.this,day_x+"/"+month_x+"/"+year_x, Toast.LENGTH_SHORT).show();
+        }
+    };
 
 }
