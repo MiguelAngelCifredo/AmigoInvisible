61fcc1370d54513ca09f31834961a69cf4e5e034
MiguelAngelCifredo <macifredo@gmail.com>
2016-04-30 16:44:29 +0200

    Corregido Contactos. Orden en la creación del objeto cuando se obtienen los contactos.

--- app/src/main/java/utils/Contactos.java
+++ app/src/main/java/utils/Contactos.java
@@ -20,42 +20,7 @@ public class Contactos {
 
     public static ArrayList<ClsPerson> listContacts;
 
-    public static void findContacts(Activity activity){
-
-        ArrayList<ClsPerson> res = new ArrayList<>();
-
-        ContentResolver cr = activity.getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
-        if (cur.getCount() > 0) {
-            while (cur.moveToNext()) {
-                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-                Cursor cur1 = cr.query(
-                        ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
-                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
-                        new String[]{id}, null);
-                while (cur1.moveToNext()) {
-                    String name  = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                    String email = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-
-                    byte[] array = cur1.getBlob(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Photo.PHOTO));
-                    Bitmap photo = null;
-                    if (array != null) {
-                        photo = BitmapFactory.decodeByteArray(array, 0, array.length);
-                    }
-                    if(email!=null && email.contains("@gmail.com")){
-                        res.add(new ClsPerson(0, email, name, photo));
-                    }
-                }
-                cur1.close();
-            }
-        }
-
-        Collections.sort(res, new PersonName());
-
-        listContacts = res;
-    }
-    // FUNCIONA PERFECTAMENTE
-    public static void readContacts(Activity activity) {
+    public static void findContacts(Activity activity) {
         ArrayList<ClsPerson> res = new ArrayList<>();
 
         ContentResolver cr = activity.getContentResolver();
@@ -105,7 +70,7 @@ public class Contactos {
                 }
                 // solo añade si el contacto es de gmail
                 if(emailContact.contains("@gmail.com")) {
-                    res.add(new ClsPerson(0,name, emailContact, bitmap));
+                    res.add(new ClsPerson(0, emailContact, name, bitmap));
                 }
             }
         }
