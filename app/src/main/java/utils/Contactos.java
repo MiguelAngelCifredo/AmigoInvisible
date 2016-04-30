ed96f49685344cf710c70a94ea363aac2f1068d7
albertogarciacobo <albertosecundario@gmail.com>
2016-04-30 18:03:53 +0200

    findContacts2
    soluciona los que los email y las fotos no coincidian con los nombres de
    los contactos

--- app/src/main/java/utils/Contactos.java
+++ app/src/main/java/utils/Contactos.java
@@ -76,6 +76,53 @@ public class Contactos {
         }
         Collections.sort(res, new PersonName());
         listContacts = res;
-    }// end readContacts()
+    }// end findContacts()
+
+    public ArrayList<ClsPerson> findContacts2(Activity activity){
+
+        ArrayList<ClsPerson> res = new ArrayList<>();
+        Bitmap photo = null;
+
+        ContentResolver cr = activity.getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                Cursor cur1 = cr.query(
+                        ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                        new String[]{id}, null);
+                while (cur1.moveToNext()) {
+                    //to get the contact names, emails and photos
+                    String name=cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String email = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                    String image_uri = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+
+                    // bitmap de la imagen
+                    if (image_uri != null) {
+                        //System.out.println(Uri.parse(image_uri));
+                        try {
+                            photo = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), Uri.parse(image_uri));
+                        } catch (FileNotFoundException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                    else photo = null;
+
+
+                    if(email!=null && email.contains("@gmail.com")){
+                        //System.out.println("nombre: " + name + " @@@ email: "+ email);
+                        res.add(new ClsPerson(0, email, name, photo));
+                    }
+                }
+                cur1.close();
+            }
+        }
+        return res;
+    }// end findContacts2()
 
 }
