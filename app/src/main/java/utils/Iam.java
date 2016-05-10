86fa93e9ca2d617b4e03fffc6f26973e97fde32a
albertogarciacobo <albertosecundario@gmail.com>
2016-05-10 18:59:22 +0200

    Funciones de registro
    fuciones de registro en DB incompletas todavía a la falta de saber sus
    nombres

--- app/src/main/java/utils/Iam.java
+++ app/src/main/java/utils/Iam.java
@@ -7,8 +7,12 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 import model.ClsEvent;
+import model.ClsPerson;
 
 public class Iam {
     private static Integer id_person = null;
@@ -58,15 +62,88 @@ public class Iam {
         } else {
             android.accounts.AccountManager am = android.accounts.AccountManager.get(activity);
             android.accounts.Account[] accounts = am.getAccounts();
+
             for(Account acc : accounts) {
-                if(acc.type.matches("com.google")) eMail = acc.name.toString();
+                if(acc.type.matches("com.google")) {
+                    eMail = acc.name.toString();
+                    /*
+                    String name = acc.name;
+                    System.out.println("@@@@@@@@@@@" + acc);
+                    Log.i("@@@@@@@ acc:", acc.toString());
+                    */
+
+                }
             }
+
         }
+        Log.i("@@@@@@@ email", eMail);
         return eMail;
     }
 
     public static boolean admin(ClsEvent eventActual){
         return eventActual.getData_id_admin() == Iam.getId();
     }
+    private static void setIam(Activity activity){
+        /*
+        mediante el método Contactos.getContacts(Activity activity) obtenemos la lista entera de los contactos.
+        Posteriormente lo comparamos con Iam.gMailAccount(Activity activity) que es nuestro email y obtenemos
+        todos los datos de la clase ClsPerson.
+         */
+
+        String myEmail = Iam.gMailAccount(activity);
+        Boolean existoEnDB = Iam.estoyRegistradoEnDB(myEmail);
+
+        // Si no existo en la DB, obtiene mis datos personales del terminal y los envia a la DB.
+        if (existoEnDB.equals(false)){
+            ClsPerson misDatos = Iam.obtainMyData(Iam.gMailAccount(activity), Contactos.getContacts(activity));
+            // modifica los datos
+            Iam.eMail     = misDatos.getData_email();
+            Iam.name      = misDatos.getData_name();
+            Iam.photo     = misDatos.getData_photo();
+            Iam.id_person = registraEnDB(Iam.name, Iam.eMail, Iam.photo);
+        }
+        // Si resulta que SI estoy registrado en la DB, obtengo los datos de ella.
+        else{
+            /*
+            Iam.eMail     = myEmail;
+            Iam.name      = getDBmyName(Iam.eMail);
+            Iam.photo     = getDBmyPhoto(Iam.eMail);
+            Iam.id_person = getDBmyID(Iam.eMail);
+            */
+
+        }
+
+
+    }// end setIam()
+
+    private static Integer registraEnDB(String name, String email, Bitmap photo) {
+        /*
+        registra los datos en la DB y devuelve el ID de la pkey asignado
+         */
+
+        return null;
+    }
+
+    private static Boolean estoyRegistradoEnDB(String email) {
+        /*
+        Devuelve true si existo en la DB o falso si no
+         */
+        Boolean res = false;
+        return res;
+    }
+
+    private static ClsPerson obtainMyData(String miEmail, ArrayList<ClsPerson> lp){
+        /*
+        de la lista de contactos del movil devuelve el contacto cuyo email es el nuestro,
+        si no, devuelve un ClsPerson nulo. 
+         */
+        
+        // como res hacemos un ClsPerson nulo. 
+        ClsPerson res =  new ClsPerson(-1, "", "John Doe", null);
+        for(ClsPerson persona: lp){
+            if(persona.getData_email().equals(miEmail)) {res = persona;}
+        }
+        return res;
+    }// end obtainMyData()
 
 }
